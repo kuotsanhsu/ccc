ea499d288878cf594d0a630d8d783f6e4a1f9ed1
kuotsanhsu <learningstud@gmail.com>
2025-06-02 01:55:21 +0800

    Add rfc3629 testcases and refactor C++ testcases with a huge loop

--- src/test_unicode_more.cc
+++ src/test_unicode_more.cc
@@ -1,89 +1,6 @@
 #include "unicode.h"
 #include <cassert>
 #include <string_view>
-using namespace std::string_view_literals;
-
-void assert_code_points(std::ranges::input_range auto &&code_points,
-			std::u8string_view code_units)
-{
-	struct utf8_iter it = {code_units.begin(), code_units.end()};
-	for (const int code_point : code_points) {
-		assert(utf8_getc(&it) == code_point);
-	}
-	assert(utf8_getc(&it) == -1);
-}
-
-/** The UTF-8 code unit sequence <41 C3 B1 42> is well-formed, because it can be
- * partitioned into subsequences, all of which match the specification for UTF-8
- * in Table 3-7. It consists of the following minimal well-formed code unit
- * subsequences: <41>, <C3 B1>, and <42>.
- */
-void well_formed_D86_01()
-{
-	assert_code_points(std::initializer_list{0x41, 0xF1, 0x42},
-			   u8"\x41\xC3\xB1\x42"sv);
-}
-
-/** The UTF-8 code unit sequence <41 C2 C3 B1 42> is ill-formed, because it
- * contains one ill-formed subsequence. There is no subsequence for the C2 byte
- * which matches the specification for UTF-8 in Table 3-7. The code unit
- * sequence is partitioned into one minimal well-formed code unit subsequence,
- * <41>, followed by one ill-formed code unit subsequence, <C2>, followed by two
- * minimal well-formed code unit subsequences, <C3 B1> and <42>.
- */
-void ill_formed_D86_02()
-{
-	assert_code_points(std::initializer_list{0x41, -3, 0xF1, 0x42},
-			   u8"\x41\xC2\xC3\xB1\x42"sv);
-}
-
-/** In isolation, the UTF-8 code unit sequence <C2 C3> would be ill-formed, but
- * in the context of the UTF-8 code unit sequence <41 C2 C3 B1 42>, <C2 C3> does
- * not constitute an ill-formed code unit subsequence, because the C3 byte is
- * actually the first byte of the minimal well-formed UTF-8 code unit
- * subsequence <C3 B1>. Ill-formed code unit subsequences do not overlap with
- * minimal well-formed code unit subsequences. */
-void ill_formed_D86_03()
-{
-	assert_code_points(std::initializer_list{-3, -2}, u8"\xC2\xC3"sv);
-}
-
-/* In UTF-8, the code point sequence <004D, 0430, 4E8C, 10302> is represented as
- * <4D D0 B0 E4 BA 8C F0 90 8C 82>, where <4D> corresponds to U+004D, <D0 B0>
- * corresponds to U+0430, <E4 BA 8C> corresponds to U+4E8C, and <F0 90 8C 82>
- * corresponds to U+10302.
- */
-void well_formed_D92_01()
-{
-	assert_code_points(
-	    std::initializer_list{0x004D, 0x0430, 0x4E8C, 0x10302},
-	    u8"\x4D\xD0\xB0\xE4\xBA\x8C\xF0\x90\x8C\x82"sv);
-}
-
-/** The byte sequence <C0 AF> is ill-formed, because C0 is not well-formed in
- * the “First Byte” column.
- */
-void ill_formed_first_byte()
-{
-	assert_code_points(std::initializer_list{-3, -3}, u8"\xC0\xAF"sv);
-}
-
-/** The byte sequence <E0 9F 80> is ill-formed, because in the row where E0 is
- * well-formed as a first byte, 9F is not well-formed as a second byte.
- */
-void ill_formed_second_byte()
-{
-	assert_code_points(std::initializer_list{-3, -3}, u8"\xE0\x9F\x80"sv);
-}
-
-/** The byte sequence <F4 80 83 92> is well-formed, because every byte in that
- * sequence matches a byte range in a row of the table (the last row).
- */
-void well_formed_four_bytes()
-{
-	assert_code_points(std::initializer_list{0x1000D2},
-			   u8"\xF4\x80\x83\x92"sv);
-}
 
 void disallowed_byte_range(char8_t first_code_unit, char8_t last_code_unit)
 {
@@ -108,15 +25,37 @@ void disallowed_byte_values()
 	disallowed_byte_range(0xF5, 0xFF);
 }
 
+#ifdef assert
+#undef assert
+#define assert(e) (__builtin_expect(!(e), 0) ? __builtin_trap() : (void)0)
+#endif
+
 int main()
 {
-	static_assert(sizeof(char8_t) == 1, "the size of char8_t must be 1");
-	well_formed_D86_01();
-	ill_formed_D86_02();
-	ill_formed_D86_03();
-	well_formed_D92_01();
-	ill_formed_first_byte();
-	ill_formed_second_byte();
-	well_formed_four_bytes();
 	disallowed_byte_values();
+	for (const auto &[code_units, code_points] : std::initializer_list<
+		 std::pair<std::u8string_view, std::initializer_list<int>>>{
+		 {u8"\x41\xC3\xB1\x42", {0x41, 0xF1, 0x42}},
+		 {u8"\x41\xC2\xC3\xB1\x42", {0x41, -3, 0xF1, 0x42}},
+		 {u8"\xC2\xC3", {-3, -2}},
+		 {u8"\x4D\xD0\xB0\xE4\xBA\x8C\xF0\x90\x8C\x82",
+		  {0x004D, 0x0430, 0x4E8C, 0x10302}},
+		 {u8"\xC0\xAF", {-3, -3}},
+		 {u8"\xE0\x9F\x80", {-3, -3}},
+		 {u8"\xF4\x80\x83\x92", {0x1000D2}},
+		 // rfc3629
+		 {u8"\x41\xE2\x89\xA2\xCE\x91\x2E",
+		  {0x0041, 0x2262, 0x0391, 0x002E}},
+		 {u8"\xED\x95\x9C\xEA\xB5\xAD\xEC\x96\xB4",
+		  {0xD55C, 0xAD6D, 0xC5B4}},
+		 {u8"\xE6\x97\xA5\xE6\x9C\xAC\xE8\xAA\x9E",
+		  {0x65E5, 0x672C, 0x8A9E}},
+		 {u8"\xEF\xBB\xBF\xF0\xA3\x8E\xB4", {0xFEFF, 0x233B4}},
+	     }) {
+		struct utf8_iter it = {code_units.begin(), code_units.end()};
+		for (const int code_point : code_points) {
+			assert(utf8_getc(&it) == code_point);
+		}
+		assert(utf8_getc(&it) == -1);
+	}
 }
