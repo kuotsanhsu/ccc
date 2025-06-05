03b94c927395053bae1c5da63ba7014bd5ff3046
kuotsanhsu <learningstud@gmail.com>
2025-06-05 17:38:07 +0800

    json_syntax: extract header

--- src/json_syntax.c
+++ src/json_syntax.c
@@ -1,25 +1,6 @@
+#include "json_syntax.h"
 #include <assert.h>
 #include <ctype.h>
-#include <stddef.h>
-
-typedef unsigned char char8_t;
-struct codepoint_stream;
-int get_codepoint(struct codepoint_stream *);
-struct codepoint_stream *utf8_stream(const char8_t *, size_t);
-
-enum json_error {
-	JSON_ERR_lex_value = -10,
-	JSON_ERR_lex_literal = -11,
-	JSON_ERR_lex_object_member = -12,
-	JSON_ERR_lex_object_name_separator = -13,
-	JSON_ERR_lex_end_object_or_value_separator = -14,
-	JSON_ERR_lex_end_array_or_value_separator = -15,
-	JSON_ERR_lex_xdigit = -16,
-	JSON_ERR_lex_digit = -17,
-	JSON_ERR_lex_number = -18,
-	JSON_ERR_lex_escape = -19,
-	JSON_ERR_lex_string = -20,
-};
 
 static int lex_whitespace(struct codepoint_stream *source, int c)
 {
@@ -266,7 +247,7 @@ static int lex_value(struct codepoint_stream *source, int c)
 	return JSON_ERR_lex_value;
 }
 
-static int lex_json_text(struct codepoint_stream *source)
+int lex_json_text(struct codepoint_stream *source)
 {
 	int c = get_codepoint(source);
 	if (c == 0xFEFF) // Skip BOM.
