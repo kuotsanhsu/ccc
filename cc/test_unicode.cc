e7f7909197a1c8981cf36a16dc9b546cf4350ed0
kuotsanhsu <learningstud@gmail.com>
2025-06-11 12:21:33 +0800

    test_unicode.hh: paste-in unicode.hh and refactor with ranges

--- cc/test_unicode.cc
+++ cc/test_unicode.cc
@@ -1,24 +1,153 @@
-#include "unicode.hh"
 #include <algorithm>
 #include <cassert>
+#include <ranges>
 #include <string_view>
 
-static_assert(std::input_iterator<u8iterator<std::u8string_view>>);
+template <std::ranges::input_range R>
+  requires std::same_as<std::ranges::range_value_t<R>, char8_t>
+class codepoint_view : public std::ranges::view_interface<codepoint_view<R>> {
+  R code_units;
 
-template <typename R> constexpr u8iterator<R> next(u8iterator<R> it) { return ++it; }
+public:
+  class iterator;
+  constexpr codepoint_view(R code_units) noexcept : code_units(std::move(code_units)) {}
+  constexpr iterator begin() const {
+    return {std::ranges::begin(code_units), std::ranges::end(code_units)};
+  }
+  constexpr std::default_sentinel_t end() const noexcept { return std::default_sentinel; }
+};
+
+template <std::ranges::input_range R>
+  requires std::same_as<std::ranges::range_value_t<R>, char8_t>
+class codepoint_view<R>::iterator {
+  std::ranges::iterator_t<R> code_unit_iter;
+  std::ranges::sentinel_t<R> code_unit_end;
+  int codepoint;
+  static constexpr int eof = -1;
+  /** Could throw in the case of reading from stdin. */
+  constexpr int next();
+
+public:
+  using difference_type = std::ptrdiff_t;
+  using value_type = int;
+  constexpr iterator(std::ranges::iterator_t<R> code_unit_iter,
+                     std::ranges::sentinel_t<R> code_unit_end)
+      : code_unit_iter(std::move(code_unit_iter)), code_unit_end(std::move(code_unit_end)),
+        codepoint(next()) {}
+  constexpr int operator*() const noexcept { return codepoint; }
+  constexpr iterator &operator++() {
+    codepoint = next();
+    return *this;
+  }
+  constexpr iterator operator++(int) {
+    auto old = *this;
+    ++*this;
+    return old;
+  }
+  constexpr bool operator==(std::default_sentinel_t) const noexcept { return codepoint == eof; }
+};
+
+template <std::ranges::input_range R>
+  requires std::same_as<std::ranges::range_value_t<R>, char8_t>
+constexpr int codepoint_view<R>::iterator::next() {
+  if (code_unit_iter == code_unit_end) {
+    return eof;
+  }
+  const int a = *code_unit_iter++;
+  if (a < 0x80) {
+    // 00..7F
+    // 0xxx'xxxx
+    return a;
+  }
+  if (a < 0xC2 || 0xF4 < a) {
+    return -3;
+  }
+  if (code_unit_iter == code_unit_end) {
+    return -2;
+  }
+  const int b = *code_unit_iter ^ 0x80;
+  if (b >> 6) {
+    return -3;
+  }
+  ++code_unit_iter;
+  int codepoint = a << 6 ^ b;
+  if (a < 0xE0) {
+    // C2..DF     80..BF
+    // 110x'xxxx  10xx'xxxx
+    return codepoint ^ (0xC0 << 6);
+  }
+
+  switch (a) {
+  case 0xE0: // E0  A0..BF  80..BF
+    if (b < 0x20) {
+      return -3;
+    }
+    break;
+  case 0xED: // ED  80..9F  80..BF
+    if (b >= 0x20) {
+      return -3;
+    }
+    break;
+  case 0xF0: // F0  90..BF  80..BF  80..BF
+    if (b < 0x10) {
+      return -3;
+    }
+    break;
+  case 0xF4: // F4  80..8F  80..BF  80..BF
+    if (b >= 0x10) {
+      return -3;
+    }
+    break;
+  }
+
+  if (code_unit_iter == code_unit_end) {
+    return -2;
+  }
+  const int c = *code_unit_iter ^ 0x80;
+  if (c >> 6) {
+    return -3;
+  }
+  ++code_unit_iter;
+  codepoint = (codepoint << 6) ^ c;
+  if (a < 0xF0) {
+    // E0         A0..BF     80..BF
+    // E1..EC     80..BF     80..BF
+    // ED         80..9F     80..BF
+    // EE..EF     80..BF     80..BF
+    // 1110'xxxx  10xx'xxxx  10xx'xxxx
+    return codepoint ^ (0xE0 << 12);
+  }
+
+  if (code_unit_iter == code_unit_end) {
+    return -2;
+  }
+  const int d = *code_unit_iter ^ 0x80;
+  if (d >> 6) {
+    return -3;
+  }
+  ++code_unit_iter;
+  codepoint = (codepoint << 6) ^ d;
+  // F0         90..BF     80..BF     80..BF
+  // F1..F3     80..BF     80..BF     80..BF
+  // F4         80..8F     80..BF     80..BF
+  // 1111'0xxx  10xx'xxxx  10xx'xxxx  10xx'xxxx
+  return codepoint ^ (0xF0 << 18);
+}
+
+constexpr inline struct to_codepoint : std::ranges::range_adaptor_closure<to_codepoint> {
+  template <std::ranges::input_range R>
+    requires std::same_as<std::ranges::range_value_t<R>, char8_t>
+  [[nodiscard]] constexpr auto operator()(R &&code_units) const {
+    return codepoint_view(std::forward<R>(code_units));
+  }
+} to_codepoint;
+
+static_assert(std::ranges::input_range<codepoint_view<std::u8string_view>>);
+static_assert(std::input_iterator<codepoint_view<std::u8string_view>::iterator>);
 
 int main() {
   using namespace std::string_view_literals;
-  constexpr u8iterator h(u8"hello world"sv);
-  static_assert(*h == 'h');
-  constexpr auto e = next(h);
-  static_assert(*e == 'e');
-  constexpr auto l = next(e);
-  static_assert(*l == 'l');
-  constexpr auto o = next(next(l));
-  for (auto it = o; const auto c : u8"o world"sv) {
-    assert(*it++ == c);
-  }
+  static_assert(std::ranges::equal(u8"hello world"sv | to_codepoint, U"hello world"sv));
 
   /* As a consequence of the well-formedness conditions specified in [Table
    * 3-7](https://www.unicode.org/versions/Unicode16.0.0/core-spec/chapter-3/#G27506), the following
@@ -29,10 +158,9 @@ int main() {
            std::ranges::iota_view(u8'\xF5', u8'\x00'),
        }) {
     for (const auto code_unit : code_units) {
-      const auto begin = &code_unit;
-      u8iterator it(std::ranges::subrange(begin, std::next(begin)));
-      assert(*it++ == -3);
-      assert(*it == it.sentinal);
+      const char8_t singleton[] = {code_unit};
+      constexpr int bad[] = {-3};
+      assert(std::ranges::equal(std::views::all(singleton) | to_codepoint, bad));
     }
   }
 
@@ -51,7 +179,6 @@ int main() {
            {u8"\xE6\x97\xA5\xE6\x9C\xAC\xE8\xAA\x9E", {0x65E5, 0x672C, 0x8A9E}},
            {u8"\xEF\xBB\xBF\xF0\xA3\x8E\xB4", {0xFEFF, 0x2'33B4}},
        }) {
-    const u8iterator it(code_units);
-    assert(std::ranges::equal(std::ranges::subrange(it, it.sentinal), codepoints));
+    assert(std::ranges::equal(code_units | to_codepoint, codepoints));
   }
 }
