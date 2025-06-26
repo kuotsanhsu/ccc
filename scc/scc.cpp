6eb44ac625fd8f4766a5324e327ae130afd233f2
kuotsanhsu <learningstud@gmail.com>
2025-06-27 04:05:39 +0800

    scc: no onStack boolean
    https://judge.yosupo.jp/submission/295816

--- scc/scc.cpp
+++ scc/scc.cpp
@@ -4,46 +4,40 @@
 #include <ranges>
 #include <span>
 #include <stack>
-#include <tuple>
 #include <vector>
 
-constexpr std::vector<std::vector<int>> scc(const int N,
-                                            const std::span<const std::pair<int, int>> edges) {
-  // const auto M{edges.size()};
-  class Vertex {
-    int index{-1};
-    int lowlink{-1};
+class Vertex {
+  int index{};
+  int lowlink{};
 
-  public:
-    bool onStack{false};
-    std::vector<Vertex *> successors{};
+public:
+  std::vector<Vertex *> successors{};
 
-    void visit(const int index) noexcept {
-      lowlink = this->index = index;
-      onStack = true;
-    }
-
-    void min_lowlink(const Vertex &other) { lowlink = std::min(lowlink, other.lowlink); }
-
-    [[nodiscard]] bool visited() const noexcept { return index != -1; }
-    [[nodiscard]] bool root() const noexcept { return index == lowlink; }
-  };
-  std::vector<Vertex> vertices(N);
-  for (const auto [from, to] : edges) {
-    vertices[from].successors.push_back(&vertices[to]);
+  void visit(const int index) noexcept {
+    this->index = -index;
+    lowlink = index;
   }
 
+  void min_lowlink(const Vertex &other) { lowlink = std::min(lowlink, other.lowlink); }
+
+  [[nodiscard]] bool visited() const noexcept { return index != 0; }
+  [[nodiscard]] bool root() const noexcept { return -index == lowlink; }
+  [[nodiscard]] bool onStack() const noexcept { return index < 0; }
+  void offStack() noexcept { index = lowlink; }
+};
+
+constexpr std::vector<std::vector<int>> scc(const std::span<Vertex> vertices) {
   std::stack<Vertex *> S;
   std::vector<std::vector<int>> components;
-  const auto strongconnect = [front = &vertices.front(), &vertices, &S,
+  const auto strongconnect = [front = &vertices.front(), vertices, &S,
                               &components](this auto &&self, Vertex *const v) -> void {
-    v->visit(S.size());
     S.push(v);
+    v->visit(S.size());
     for (auto w : v->successors) {
       if (!w->visited()) {
         self(w);
         v->min_lowlink(*w);
-      } else if (w->onStack) {
+      } else if (w->onStack()) {
         // Successor w is in stack S and hence in the current SCC. If w is not on stack, then (v, w)
         // is an edge pointing to an SCC already found and must be ignored.
         v->min_lowlink(*w);
@@ -54,7 +48,7 @@ constexpr std::vector<std::vector<int>> scc(const int N,
       while (true) {
         const auto w = S.top();
         S.pop();
-        w->onStack = false;
+        w->offStack();
         component.push_back(w - front);
         if (v == w) {
           break;
@@ -89,15 +83,14 @@ std::ostream &operator<<(std::ostream &os, const nested_sized_ranges auto &&resu
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  std::array<std::pair<int, int>, 500'000> edges;
-  std::istream_iterator<int> ints(std::cin);
+  Vertex vertices[500'000];
+  std::istream_iterator<size_t> ints(std::cin);
   const auto N = *ints++;
   const auto M = *ints++;
-  auto last = edges.begin();
-  for (const auto _ : std::views::iota(0, M)) {
-    const auto a = *ints++;
-    const auto b = *ints++;
-    *last++ = {a, b};
+  for (const auto _ : std::views::iota(size_t{}, M)) {
+    auto &from = vertices[*ints++];
+    auto &to = vertices[*ints++];
+    from.successors.push_back(&to);
   }
-  std::cout << std::ranges::reverse_view(scc(N, {edges.begin(), last}));
+  std::cout << std::ranges::reverse_view(scc({std::begin(vertices), N}));
 }
