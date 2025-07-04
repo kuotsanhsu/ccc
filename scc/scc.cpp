bdc8138d9a1dd9d36874e896a51535a433b06ab0
kuotsanhsu <learningstud@gmail.com>
2025-07-04 16:49:06 +0800

    scc: std::pmr::forward_list for successors
    157 ms 38.24 Mib
    https://judge.yosupo.jp/submission/297303

--- scc/scc.cpp
+++ scc/scc.cpp
@@ -1,30 +1,34 @@
 #include <array>
+#include <forward_list>
 #include <iostream>
 #include <iterator>
+#include <memory_resource>
 #include <ranges>
 #include <span>
-#include <vector>
 
 constexpr size_t max_size{500'000};
+std::array<std::byte, sizeof(nullptr) * 2 * max_size> buffer;
+std::pmr::monotonic_buffer_resource mbr(buffer.data(), buffer.size());
 
-struct Vertex {
-  Vertex **rindex{nullptr};
-  std::vector<Vertex *> successors{};
+struct vertex {
+  vertex **rindex{nullptr};
+  std::pmr::forward_list<vertex *> successors{&mbr};
+  decltype(successors)::const_iterator last_successor{successors.cbefore_begin()};
 };
 
 class scc {
-  static inline std::array<Vertex *, max_size> S;
-  const Vertex *front;
-  Vertex **stack = S.begin();
-  Vertex **component = S.end();
+  static inline std::array<vertex *, max_size> stack;
+  const vertex *front;
+  vertex **top = stack.begin();
+  vertex **component = stack.end();
   size_t K{0};
 
-  constexpr void tarjan(Vertex *const v) {
+  constexpr void tarjan(vertex *const v) {
     if (v->rindex) {
       return;
     }
-    const auto rindex = v->rindex = stack;
-    *stack++ = v;
+    const auto rindex = v->rindex = top;
+    *top++ = v;
     for (auto w : v->successors) {
       tarjan(w);
       v->rindex = std::min(v->rindex, w->rindex);
@@ -32,14 +36,14 @@ class scc {
     if (v->rindex == rindex) {
       const auto last = component;
       do {
-        (*--component = *--stack)->rindex = last;
-      } while (stack != rindex);
+        (*--component = *--top)->rindex = last;
+      } while (top != rindex);
       ++K;
     }
   }
 
 public:
-  constexpr scc(const std::span<Vertex> vertices) noexcept : front(&vertices.front()) {
+  constexpr scc(const std::span<vertex> vertices) noexcept : front(&vertices.front()) {
     for (auto &vertex : vertices) {
       tarjan(&vertex);
     }
@@ -47,7 +51,7 @@ public:
 
   friend std::ostream &operator<<(std::ostream &os, const scc &scc) {
     os << scc.K << '\n';
-    for (auto v = scc.component; v != scc.S.end();) {
+    for (auto v = scc.component; v != scc.stack.end();) {
       const auto last = (*v)->rindex;
       os << last - v;
       for (auto w = last; w != v;) {
@@ -62,14 +66,14 @@ public:
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  static std::array<Vertex, max_size> vertices;
+  static std::array<vertex, max_size> vertices;
   std::istream_iterator<size_t> ints(std::cin);
   const auto N = *ints++;
   const auto M = *ints++;
   for (const auto _ : std::views::iota(size_t{}, M)) {
     auto &from = vertices[*ints++];
     auto &to = vertices[*ints++];
-    from.successors.push_back(&to);
+    from.last_successor = from.successors.insert_after(from.last_successor, &to);
   }
   std::cout << scc({std::begin(vertices), N});
 }
