131c57040be83866ce0adfdf5117d5f1f0d1ca5e
kuotsanhsu <learningstud@gmail.com>
2026-01-02 11:04:29 +0800

    WIP

--- tarjan.cpp
+++ tarjan.cpp
@@ -1,119 +1,136 @@
 // https://judge.yosupo.jp/problem/scc
-#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <ranges>
 #include <vector>
 
-class graph {
-  std::vector<std::vector<std::size_t>> vertices;
+struct vertex;
+struct children {
+  const children *next;
+  vertex *vertex;
+};
+using stack_frame = vertex *;
+class vertex {
+  const children *children;
+  const stack_frame *low_link;
+  stack_frame *prev;
 
 public:
-  constexpr graph(std::size_t vertex_count) : vertices(vertex_count) {}
-  constexpr void push_edge(std::size_t source, std::size_t target) {
-    vertices[source].push_back(target);
+  stack_frame *stack(stack_frame *prev, stack_frame *&top) {
+    prev = prev;
+    low_link = top;
+    *top = this;
+    return top++;
   }
-  [[nodiscard]] constexpr auto vertex_count() const noexcept { return vertices.size(); }
-
-  class strong_component_view : public std::ranges::view_interface<strong_component_view> {
-    std::vector<std::size_t> sorted, reverse_component_sizes;
-    using link = decltype(sorted)::iterator;
-
-  public:
-    class iterator {
-      std::vector<std::size_t>::const_iterator acc;
-      std::vector<std::size_t>::const_reverse_iterator curr;
-
-    public:
-      using difference_type = std::ptrdiff_t;
-      using value_type = std::ranges::subrange<decltype(acc)>;
-
-      constexpr iterator(decltype(acc) acc, decltype(curr) first) noexcept
-          : acc(acc), curr(first) {}
-      constexpr value_type operator*() const noexcept { return {acc, acc + *curr}; }
-      constexpr iterator &operator++() {
-        acc += *curr++;
-        return *this;
-      }
-      constexpr iterator operator++(int) {
-        auto old = *this;
-        ++*this;
-        return old;
-      }
-      constexpr bool operator==(std::vector<std::size_t>::const_iterator last) const noexcept {
-        return acc == last;
-      };
-    };
-
-    [[nodiscard]] constexpr iterator begin() const noexcept {
-      return {sorted.begin(), reverse_component_sizes.rbegin()};
+  [[nodiscard]] bool visited() const noexcept { return low_link; }
+  bool same_low_link(const stack_frame *curr) { return low_link == curr; }
+  void update_low_link(const vertex *vertex) { low_link = std::min(low_link, vertex->low_link); }
+  void add_child(struct children *&edge_pool_ptr, vertex &child) {
+    *edge_pool_ptr = {.next = children, .vertex = &child};
+    children = edge_pool_ptr++;
+  }
+  vertex *consume_child() {
+    vertex *vertex = nullptr;
+    if (auto &child = children) {
+      vertex = child->vertex;
+      child = child->next;
     }
-    [[nodiscard]] constexpr std::vector<std::size_t>::const_iterator end() const noexcept {
-      return sorted.end();
-    }
-    [[nodiscard]] constexpr size_t size() const noexcept { return reverse_component_sizes.size(); }
-
-    constexpr strong_component_view(const graph &);
-  };
-  [[nodiscard]] constexpr strong_component_view strong_components() const { return {*this}; }
+    return vertex;
+  }
+  static stack_frame *go_back(stack_frame *&curr) { return curr = (*curr)->prev; }
 };
 
-static_assert(std::ranges::input_range<graph::strong_component_view>);
-static_assert(std::input_iterator<graph::strong_component_view::iterator>);
-static_assert(std::ranges::input_range<graph::strong_component_view::iterator::value_type>);
+class strong_component_view : public std::ranges::view_interface<strong_component_view> {
+  std::span<const stack_frame> sorted;
+  std::span<const std::ptrdiff_t> reverse_component_sizes;
 
-constexpr graph::strong_component_view::strong_component_view(const graph &graph)
-    : sorted(graph.vertex_count()) {
-  auto top = sorted.begin(), y = sorted.end();
-  std::vector<link> links(sorted.size());
-  // GCC has a bug that prevents "capturing this":
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=113563
-  auto &reverse_component_sizes = this->reverse_component_sizes;
-  const auto tarjan = [&graph, &top, &y, &links, &reverse_component_sizes](this auto &&tarjan,
-                                                                           std::size_t i) {
-    if (const auto link = links[i]; link.base()) {
-      return link;
+public:
+  class iterator {
+    std::span<const stack_frame>::iterator acc;
+    std::span<const std::ptrdiff_t>::reverse_iterator curr;
+
+  public:
+    using difference_type = std::ptrdiff_t;
+    using value_type = std::ranges::subrange<decltype(acc)>;
+
+    constexpr iterator(std::span<const stack_frame>::iterator acc,
+                       std::span<const std::ptrdiff_t>::reverse_iterator first) noexcept
+        : acc(acc), curr(first) {}
+    constexpr value_type operator*() const noexcept { return {acc, acc + *curr}; }
+    constexpr iterator &operator++() {
+      acc += *curr++;
+      return *this;
     }
-    auto low_link = links[i] = top;
-    *top++ = i;
-    for (const auto j : graph.vertices[i]) {
-      low_link = std::min(low_link, tarjan(j));
+    constexpr iterator operator++(int) {
+      auto old = *this;
+      ++*this;
+      return old;
     }
-    if (low_link == links[i]) {
-      auto x = low_link;
-      while (x != top && top != y) {
-        *--y = *x++;
-      }
-      if (top == y) {
-        std::reverse(x, top);
-        y = x;
-      }
-      for (const auto i : std::ranges::subrange(low_link, top)) {
-        links[i] = y;
-      }
-      reverse_component_sizes.push_back(top - low_link);
-      top = low_link;
-    }
-    return low_link;
+    constexpr bool operator==(std::span<const stack_frame>::iterator last) const noexcept {
+      return acc == last;
+    };
   };
 
-  std::ranges::for_each(std::views::iota(0uz, graph.vertex_count()), tarjan);
-  assert(top == sorted.begin());
-  assert(y == sorted.begin());
-}
-
-template <typename CharT, typename Traits>
-auto &operator<<(std::basic_ostream<CharT, Traits> &os,
-                 const graph::strong_component_view &strong_components) {
-  os << std::ranges::size(strong_components) << '\n';
-  for (const auto &component : strong_components) {
-    os << std::ranges::size(component);
-    for (const auto i : component) {
-      os << ' ' << i;
-    }
-    os << '\n';
+  [[nodiscard]] constexpr iterator begin() const noexcept {
+    return {sorted.begin(), reverse_component_sizes.rbegin()};
   }
-  return os;
+  [[nodiscard]] constexpr auto end() const noexcept { return sorted.end(); }
+  [[nodiscard]] constexpr size_t size() const noexcept { return reverse_component_sizes.size(); }
+
+  constexpr strong_component_view(std::span<stack_frame> sorted,
+                                  std::span<const std::ptrdiff_t> reverse_component_sizes)
+      : sorted(sorted), reverse_component_sizes(reverse_component_sizes) {}
+};
+
+static_assert(std::ranges::input_range<strong_component_view>);
+static_assert(std::input_iterator<strong_component_view::iterator>);
+static_assert(std::ranges::input_range<strong_component_view::iterator::value_type>);
+
+strong_component_view preallocated(std::span<vertex> vertices,
+                                   std::ranges::input_range auto &&edges) {
+  // log2(500'000) is about 18.897
+  constexpr auto N = 500'000;
+  constexpr auto M = 500'000;
+
+  static children edge_pool[M];
+  for (auto edge_pool_ptr = edge_pool; const auto [source, target] : edges) {
+    vertices[source].add_child(edge_pool_ptr, vertices[target]);
+  }
+
+  std::vector<std::ptrdiff_t> reverse_component_sizes;
+  static stack_frame stack[N];
+  auto sorted = stack + vertices.size();
+  for (auto top = stack; auto &vertex : vertices) {
+    assert(top == stack);
+    if (vertex.visited()) {
+      continue;
+    }
+    for (auto curr = vertex.stack(nullptr, top);;) {
+      while (auto vertex = (*curr)->consume_child()) {
+        if (vertex->visited()) {
+          (*curr)->update_low_link(vertex);
+        } else {
+          curr = vertex->stack(curr, top);
+          goto end;
+        }
+      }
+      if ((*curr)->same_low_link(curr)) {
+        reverse_component_sizes.push_back(top - curr);
+        while (top != curr) {
+          *--sorted = *--top;
+        }
+        if (!vertex::go_back(curr)) {
+          break;
+        }
+      } else {
+        auto vertex = *curr;
+        (*vertex::go_back(curr))->update_low_link(vertex);
+      }
+    end:
+    }
+  }
+  assert(sorted == stack);
+  return {std::span(sorted, vertices.size()), reverse_component_sizes};
 }
 
 namespace std {
@@ -123,59 +140,20 @@ auto &operator>>(std::basic_istream<CharT, Traits> &is, std::pair<A, B> &pair) {
 }
 } // namespace std
 
-static constexpr auto scc(std::size_t vertex_count, std::ranges::input_range auto &&edges) {
-  graph graph(vertex_count);
-  for (const auto [source, target] : edges) {
-    assert(source < vertex_count && target < vertex_count);
-    graph.push_edge(source, target);
-  }
-  return graph.strong_components();
-}
-
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  auto pairs = std::istream_iterator<std::pair<std::size_t, std::size_t>>(std::cin);
+  auto pairs = std::istream_iterator<std::pair<int, int>>(std::cin);
   const auto [vertex_count, edge_count] = *pairs++;
-  std::cout << scc(vertex_count, std::views::counted(pairs, edge_count));
-}
+  std::vector<vertex> vertices(vertex_count);
+  const auto strong_components = preallocated(vertices, std::views::counted(pairs, edge_count));
 
-template <typename T> class list {
-  struct node {
-    T value;
-    node *next;
-  };
-  node *front = nullptr;
-
-public:
-  constexpr void push_front(const T &value) {
-    node node{value, front};
-    front = &node;
+  const auto j = vertices.data();
+  std::cout << std::ranges::size(strong_components) << '\n';
+  for (const auto &component : strong_components) {
+    std::cout << std::ranges::size(component);
+    for (const auto i : component) {
+      std::cout << ' ' << &*i - j;
+    }
+    std::cout << '\n';
   }
-  constexpr void push_front(T &&value) {
-    node node{std::move(value), front};
-    front = &node;
-  }
-};
-
-static constexpr bool ranges_of_ranges_equal(auto &&r1, auto &&r2) {
-  return std::ranges::equal(r1, r2,
-                            [](auto &&r1, auto &&r2) { return std::ranges::equal(r1, r2); });
 }
-
-// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
-static_assert(ranges_of_ranges_equal(scc(6, std::array<std::pair<int, int>, 7>{{
-                                                {1, 4},
-                                                {5, 2},
-                                                {3, 0},
-                                                {5, 5},
-                                                {4, 1},
-                                                {0, 3},
-                                                {4, 2},
-                                            }}),
-                                     std::initializer_list<std::initializer_list<int>>{
-                                         {5},
-                                         {4, 1},
-                                         {2},
-                                         {3, 0},
-                                     }));
-// NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
