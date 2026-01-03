4426a8a1c22ff7d42400ba52165051f56556d7ab
kuotsanhsu <learningstud@gmail.com>
2026-01-03 19:08:00 +0800

    WIP

---
+++ reference.md
@@ -0,0 +1,31 @@
+## Algorithms
+
+- 1974, [The Design and Analysis of Computer Algorithms](https://dl.acm.org/doi/10.5555/578775)
+    - Aho, Hopcroft
+- 1983, [Data structures and algorithms](https://onlinelibrary.wiley.com/doi/10.1002/net.3230150111)
+    - Aho, Hopcroft, Ullman
+- 1992, [Foundations of Computer Science](http://infolab.stanford.edu/~ullman/focs.html)
+    - Aho, Ullman
+- 1994 2e, [Concrete Mathematics: A Foundation for Computer Science](https://www-cs-faculty.stanford.edu/~knuth/gkp.html)
+    - Graham, Knuth, Patashnik
+- 2009, [Algorithm Design: Foundation, Analysis, and Internet Example](https://dl.acm.org/doi/10.5555/1538644)
+    - Goodrich, Tamassia
+- 2015, [Mathematics for Computer Science](https://people.csail.mit.edu/meyer/mcs.pdf)
+    - Lehman, Leighton, Meyer
+    - [MIT OpenCourseWare](https://ocw.mit.edu/courses/6-042j-mathematics-for-computer-science-spring-2015/pages/readings/)
+    - [Book Review: Mathematics for Computer Science (Suggestion for MIRI Research Guide)](https://www.lesswrong.com/posts/bdvbsf8Y6FbrC2bea/book-review-mathematics-for-computer-science-suggestion-for)
+- 2015 2e, [An Introduction to the Analysis of Algorithms](https://aofa.cs.princeton.edu/home/)
+    - Sedgewick, Flajolet
+- 2022 4e, [Introduction to Algorithms](https://mitpress.mit.edu/9780262046305/introduction-to-algorithms/)
+    - Cormen, Leiserson, Rivest, Stein
+
+### Further reading
+
+- 1997, [Algorithms on Strings, Trees, and Sequences](https://doi.org/10.1017/CBO9780511574931)
+    - Dan Gusfield
+- 2019, [Mathematics and Computation: A Theory Revolutionizing Technology and Science](https://www.math.ias.edu/avi/book)
+    - Avi Wigderson
+- 2020, [Computational Complexity and Property Testing: On the Interplay Between Randomness and Computation](https://link.springer.com/book/10.1007/978-3-030-43662-9)
+    - Oded Goldreich et al.
+- 2026, [The Joy of Cryptography: An Undergraduate Course in Provable Security](https://mitpress.mit.edu/9780262049979/the-joy-of-cryptography/)
+    - Mike Rosulek
--- tarjan.cpp
+++ tarjan.cpp
@@ -1,34 +1,33 @@
 // https://judge.yosupo.jp/problem/scc
+// https://judge.yosupo.jp/submission/287255
+// https://honam0905.github.io/CP-library
 #include <cassert>
 #include <iostream>
-#include <ostream>
 #include <ranges>
 #include <vector>
 
 // log2(500'000) is about 18.897
-constexpr auto N = 500'000;
-constexpr auto M = 500'000;
+constexpr int vertex_count_max = 500'000;
+constexpr int edge_count_max = 500'000;
 
-struct vertex;
-struct children {
-  const children *next;
-  struct vertex *vertex;
-};
-using stack_frame = vertex *;
 class vertex {
-  const struct children *children;
+  using stack_frame = vertex *;
   const stack_frame *low_link;
   stack_frame *prev;
+  const struct children {
+    const children *next;
+    struct vertex *vertex;
+  } *children;
 
 public:
-  [[nodiscard]] auto index() const noexcept { return this - vertex_begin; }
+  [[nodiscard]] std::ptrdiff_t index() const noexcept { return this - vertex_begin; }
   stack_frame *stack(stack_frame *prev, stack_frame *&top) {
     this->prev = prev;
     low_link = top;
     *top = this;
     return top++;
   }
-  [[nodiscard]] bool visited() const noexcept { return low_link; }
+  [[nodiscard]] bool visited() const noexcept { return low_link != nullptr; }
   bool same_low_link(const stack_frame *curr) { return low_link == curr; }
   void update_low_link(const vertex *other) { low_link = std::min(low_link, other->low_link); }
   void bump_low_link(const stack_frame *high) {
@@ -38,8 +37,8 @@ public:
 #endif
   }
   void add_child(vertex &child) {
-    static std::array<struct children, M> edge_pool;
-    static auto edge_pool_ptr = edge_pool.begin();
+    static std::array<struct children, edge_count_max> edge_pool;
+    static auto *edge_pool_ptr = edge_pool.begin();
 
     *edge_pool_ptr = {.next = children, .vertex = &child};
     children = edge_pool_ptr++;
@@ -54,48 +53,62 @@ public:
   }
   static stack_frame *go_back(stack_frame *&curr) { return curr = (*curr)->prev; }
 
-  friend auto &operator<<(std::ostream &os, const vertex &vertex) {
-    os << '(';
-    if (vertex.prev) {
-      os << vertex.prev - low_link_begin;
+  friend auto &operator<<(std::ostream &cos, const vertex &vertex) {
+    cos << '(';
+    if (vertex.prev != nullptr) {
+      cos << vertex.prev - low_link_begin;
     } else {
-      os << '*';
+      cos << '*';
     }
-    os << " <- " << vertex.low_link - low_link_begin << ") " << vertex.index() << ':';
-    for (auto children = vertex.children; children; children = children->next) {
-      os << ' ' << children->vertex->index();
+    cos << " <- " << vertex.low_link - low_link_begin << ") " << vertex.index() << ':';
+    for (const auto *children = vertex.children; children != nullptr; children = children->next) {
+      cos << ' ' << children->vertex->index();
     }
-    return os;
+    return cos;
+  }
+
+  static std::span<vertex> get_vertices(std::size_t count) {
+    assert(count <= vertices.size());
+    return {vertices.begin(), count};
+  }
+  static std::span<stack_frame> get_stack(std::size_t count) {
+    assert(count <= dfs_stack.size());
+    return {dfs_stack.begin(), count};
   }
 
 private:
+  static constinit std::array<vertex, vertex_count_max> vertices;
   static constinit const vertex *const vertex_begin;
+  static constinit std::array<stack_frame, vertex_count_max> dfs_stack;
   static constinit const stack_frame *const low_link_begin;
 };
 
-static constinit std::array<vertex, N> vertices{};
+constinit std::array<vertex, vertex_count_max> vertex::vertices{};
 constinit const vertex *const vertex::vertex_begin = vertices.cbegin();
-static constinit std::array<stack_frame, N> dfs_stack{};
-constinit const stack_frame *const vertex::low_link_begin = dfs_stack.cbegin();
+constinit std::array<vertex::stack_frame, vertex_count_max> vertex::dfs_stack{};
+constinit const vertex::stack_frame *const vertex::low_link_begin = dfs_stack.cbegin();
 
-void preallocated(std::span<vertex> vertices, std::ranges::input_range auto &&edges) {
+namespace {
+void preallocated(std::size_t vertex_count, std::ranges::input_range auto &&edges) {
+  auto vertices = vertex::get_vertices(vertex_count);
   for (const auto [source, target] : edges) {
     vertices[source].add_child(vertices[target]);
   }
 
   std::vector<std::ptrdiff_t> reverse_component_sizes;
-  auto sorted = dfs_stack.begin() + vertices.size();
-  for (auto top = dfs_stack.begin(); auto &vertex : vertices) {
-    assert(top == dfs_stack.begin());
+  auto dfs_stack = vertex::get_stack(vertex_count);
+  auto *sorted = dfs_stack.end().base();
+  for (auto *top = dfs_stack.begin().base(); auto &vertex : vertices) {
+    // assert(top == dfs_stack.begin());
     // __builtin_debugtrap();
     if (vertex.visited()) {
       continue;
     }
-    for (auto curr = vertex.stack(nullptr, top);;) {
+    for (auto *curr = vertex.stack(nullptr, top);;) {
 #ifndef NDEBUG
-      std::clog << **curr << std::endl;
+      std::clog << **curr << '\n';
 #endif
-      while (auto vertex = (*curr)->consume_child()) {
+      while (auto *vertex = (*curr)->consume_child()) {
         if (vertex->visited()) {
           (*curr)->update_low_link(vertex);
         } else {
@@ -112,21 +125,19 @@ void preallocated(std::span<vertex> vertices, std::ranges::input_range auto &&ed
           (*--sorted = *--top)->bump_low_link(sorted);
         }
 #ifndef NDEBUG
-        std::clog << std::endl;
+        std::clog << '\n';
 #endif
         if (!vertex::go_back(curr)) {
           break;
         }
       } else {
-        auto vertex = *curr;
-        vertex::go_back(curr);
-        assert(curr);
-        (*curr)->update_low_link(vertex);
+        auto *vertex = *curr;
+        (*vertex::go_back(curr))->update_low_link(vertex);
       }
     end:
     }
   }
-  assert(sorted == dfs_stack.begin());
+  // assert(sorted == dfs_stack.begin());
 
   std::cout << reverse_component_sizes.size() << '\n';
   for (auto component_size : std::views::reverse(reverse_component_sizes)) {
@@ -137,17 +148,18 @@ void preallocated(std::span<vertex> vertices, std::ranges::input_range auto &&ed
     std::cout << '\n';
   }
 }
+} // namespace
 
 namespace std {
-template <typename CharT, typename Traits, typename A, typename B>
-auto &operator>>(std::basic_istream<CharT, Traits> &is, std::pair<A, B> &pair) {
-  return is >> pair.first >> pair.second;
+template <typename A, typename B> auto &operator>>(std::istream &cis, std::pair<A, B> &pair) {
+  return cis >> pair.first >> pair.second;
 }
 } // namespace std
 
 int main() {
-  std::cin.tie(nullptr)->sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
   std::istream_iterator<std::pair<int, int>> pairs(std::cin);
   const auto [vertex_count, edge_count] = *pairs++;
-  preallocated(std::span(vertices.begin(), vertex_count), std::views::counted(pairs, edge_count));
+  preallocated(vertex_count, std::views::counted(pairs, edge_count));
 }
